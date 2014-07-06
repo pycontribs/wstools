6527f64e5d54a4963f5826dff02a3e0fb67275f1
Julien Iguchi-Cartigny <kartoch@gmail.com>
2014-07-06 21:22:46 +0200

    trying with __test__ to remove tests detected by nose

--- tests/test_wsdl.py
+++ tests/test_wsdl.py
@@ -27,14 +27,14 @@ def makeTestSuite(section='services_by_file'):
     suite = unittest.TestSuite()
     for i in range(0, numTests):
         case = WSDLToolsAbstractCase()
-        case.__test__ = true
+        case.__test__ = True
         suite.addTest(unittest.makeSuite(case, 'test_'))
     return suite
 
 
 class WSDLToolsAbstractCase(unittest.TestCase):
 
-    __test__ = false
+    __test__ = False
 
     def __init__(self, methodName='runTest'):
         unittest.TestCase.__init__(self, methodName)
