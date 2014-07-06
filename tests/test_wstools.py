475c16c97048013fd65e6cd415dc81eb2749a84f
Julien Iguchi-Cartigny <kartoch@gmail.com>
2014-07-06 19:44:30 +0200

    removing useless tar.gz, adding two new schemas from OVH API for testing

--- tests/test_wstools.py
+++ tests/test_wstools.py
@@ -6,26 +6,11 @@
 ###########################################################################
 
 import unittest
-import tarfile
-import os
-import ConfigParser
 import test_wsdl
 
 
 SECTION = 'files'
 CONFIG_FILE = 'config.txt'
-
-
-def extractFiles(section, option):
-    config = ConfigParser.ConfigParser()
-    config.read(CONFIG_FILE)
-    archives = config.get(section, option)
-    archives = eval(archives)
-    for file in archives:
-        tar = tarfile.open(file)
-        if not os.access(tar.membernames[0], os.R_OK):
-            for i in tar.getnames():
-                tar.extract(i)
 
 
 def makeTestSuite():
@@ -35,7 +20,6 @@ def makeTestSuite():
 
 
 def main():
-    extractFiles(SECTION, 'archives')
     unittest.main(defaultTest="makeTestSuite")
 
 if __name__ == "__main__":
