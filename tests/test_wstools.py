cf048dd8e93a56ef663d242a717990ea26c350c1
Julien Iguchi-Cartigny <kartoch@gmail.com>
2014-07-06 20:11:16 +0200

    make test_wstools detected by nose

--- tests/test_wstools.py
+++ tests/test_wstools.py
@@ -8,19 +8,11 @@
 import unittest
 import test_wsdl
 
-
-SECTION = 'files'
-CONFIG_FILE = 'config.txt'
-
-
 def makeTestSuite():
     suite = unittest.TestSuite()
     suite.addTest(test_wsdl.makeTestSuite("services_by_file"))
     return suite
 
-
-def main():
-    unittest.main(defaultTest="makeTestSuite")
-
 if __name__ == "__main__":
-    main()
+    runner = unittest.TextTestRunner()
+    runner.run(makeTestSuite())
